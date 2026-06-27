f3d331faa02500cbc9dd1203b6acb592172cca80
Aman <amankrdev28@gmail.com>
2026-06-27 21:12:10 +0530

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-300 bg-gray-800 flex  justify-center">
+        tailwind test
+      </h1>
+    </>
+  );
+}
+
+export default App;
